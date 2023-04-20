f9aa089948bf22d97aadbb41f4a7fa7bca4179a1
chiraz Litim <chiraz.litim@ensia.edu.dz>
2023-04-20 20:15:59 +0200

    adding Modifications

--- Quranic_school_system/admin/inc/navbar.php
+++ Quranic_school_system/admin/inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="../logo.png" width="40">
+      <img src="../assets/img/Screenshot_2023-04-18_011830-removebg-preview_1.png" alt="" class="img-fluid" width="40">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,42 +9,42 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0"
           id="navLinks">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" 
              aria-current="page" 
              href="index.php">Dashboard</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="teacher.php">الاساتذة</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="teacher.php">Teachers</a>
+          <a class="nav-link" href="#contact">التلاميذ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Students</a>
+          <a class="nav-link" href="#contact">مكتب التسجيل</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Registrar-Office</a>
+          <a class="nav-link" href="#contact">القسم</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Class</a>
+          <a class="nav-link" href="#contact">الطور</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Section</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">Schedule</a>
+          <a class="nav-link" href="#contact">استعمال الزمن</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#contact">Course</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Message</a>
+          <a class="nav-link" href="#contact">رسالة</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Settings</a>
+          <a class="nav-link" href="#contact">الاعدادت</a>
         </li>
       </ul>
       <ul class="navbar-nav me-right mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="../logout.php">Logout</a>
+          <a class="nav-link" href="../logout.php">تسجيل الخروج</a>
         </li>
       </ul>
   </div>
