e4325dfd81a0783ed2c8e4b2b2479487a9d2535d
Thouria Tahari <thouria.tahari@ensia.edu.dz>
2023-04-20 23:36:33 +0100

    adding student feature of Admin

--- admin/inc/navbar.php
+++ admin/inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="../assets/img/Screenshot_2023-04-18_011830-removebg-preview_1.png" alt="" class="img-fluid" width="40">
+      <img src="../logo.png" width="40">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,37 +9,37 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0"
           id="navLinks">
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link" 
              aria-current="page" 
-             href="index.php">Dashboard</a>
-        </li> -->
+             href="index.php">الصفحة الرئيسية</a>
+        </li>
         <li class="nav-item">
-          <a class="nav-link" href="teacher.php">الاساتذة</a>
+          <a class="nav-link" href="teacher.php">الأساتذة</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#contact">التلاميذ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">مكتب التسجيل</a>
+          <a class="nav-link" href="#contact">مكتب التسجيلات</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">القسم</a>
+          <a class="nav-link" href="#contact">الأقسام</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">الطور</a>
+          <a class="nav-link" href="#contact">المستويات</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">استعمال الزمن</a>
+          <a class="nav-link" href="#contact">التوقيت الدراسي</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">Course</a>
+          <a class="nav-link" href="#contact">الحصص</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">رسالة</a>
+          <a class="nav-link" href="#contact">الرسائل</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#contact">الاعدادت</a>
+          <a class="nav-link" href="#contact">الإعدادات</a>
         </li>
       </ul>
       <ul class="navbar-nav me-right mb-2 mb-lg-0">
